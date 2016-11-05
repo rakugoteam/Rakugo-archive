782033fc40721676707c5c4ead5d86992fcb95e1
jeremi360 <jeremi360@gmail.com>
2016-11-05 15:58:29 +0100

    Gui in Game Done (Graphics only)

---
+++ scripts/RenGD/api.gd
@@ -0,0 +1,13 @@
+
+extends Node
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
