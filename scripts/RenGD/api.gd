33b83c896c48dcbafcf98c8bac3f745de94df5d0
jeremi360 <jeremi360@gmail.com>
2016-11-13 11:08:19 +0100

    smoe more progress on sentence passer

--- scripts/RenGD/api.gd
+++ scripts/RenGD/api.gd
@@ -8,20 +8,45 @@ onready var say_screen = get_node(say_path + "Dialog")
 onready var namebox_screen = get_node(say_path + "NameBox/Label")
 
 func _ready():
-	say("developer", "This is {b}just{/b} a {i}test{/i}.")
+	say("developer", "This is {b}just{/b} a {i}test{/i} \n\n [1234].")
 
 func say(how, what):
 	print("clear say screen")
 	say_screen.clear()
 	namebox_screen.clear()
-	
+
 	print("setting say screen text")
 	namebox_screen.set_bbcode(sentence_passer(how))
 	say_screen.set_bbcode(sentence_passer(what))
 
 func sentence_passer(text):
 	print("parse text")
+
+	var	pstr = ""
+	var vstr = ""
+	var b = false
+
+	var i = 1
+
+	for t in text:
+
+		if t == "[":
+			b = true
+			pstr += t
+
+		elif t == "]":
+			b = false
+			pstr += t
+			vstr = var2str(str2var(vstr))
+			print(pstr, " ", vstr)
+			text = text.replace(pstr, vstr)
+
+		elif b:
+			pstr += t
+			vstr += t
+
+	#text = text.replace("image")
 	text = text.replace("{", "[")
 	text = text.replace("}", "]")
+
 	return text
-	 
