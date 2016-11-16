97a4c5dce95c9e1d8f40a7b3801cc4a67118817a
jeremi360 <jeremi360@gmail.com>
2016-11-16 12:57:04 +0100

    fished renpy.input equivalent, make define equivalent, make global vars lists and dict

---
+++ scripts/RenGD/renapiraw.gd
@@ -0,0 +1,51 @@
+
+extends Node
+
+# This is Ren'GD API
+# version: 0.01
+
+var characters = []
+var screens = []
+var images = []
+var vars = {}
+
+func _ready():
+  Globals.set("characters", characters)
+  Globals.set("screens", screens)
+  Globals.set("images", images)
+  Globals.set("vars", vars)
+
+func define(var_name, var_value):
+  Globals.get("vars")[var_name] = var_value
+
+func _get_var(var_name):
+  return Globals.get("vars")[var_name]
+
+func sentence_passer(text):
+	var pstr = ""
+	var vstr = ""
+	var b = false
+
+	var i = 1
+
+	for t in text:
+
+		if t == "[":
+			b = true
+			pstr += t
+
+		elif t == "]":
+			b = false
+			pstr += t
+			vstr = var2str(_get_var(vstr))
+			text = text.replace(pstr, vstr)
+
+		elif b:
+			pstr += t
+			vstr += t
+
+	text = text.replace("{image", "[img")
+	text = text.replace("{", "[")
+	text = text.replace("}", "]")
+
+	return text
--- scripts/RenGD/say_screen.gd
+++ scripts/RenGD/say_screen.gd
@@ -5,72 +5,30 @@ extends Control
 # version: 0.01
 var _bbcode = ""
 var _say_path = "VBoxContainer/"
-onready var _say_screen = get_node(say_path + "Dialog")
-onready var _namebox_screen = get_node(say_path + "NameBox/Label")
-onready var _input = get_node(say_path + "EditText")
+
+onready var _ren = get_node("/root/renapiraw")
+onready var _say_screen = get_node(_say_path + "Dialog")
+onready var _namebox_screen = get_node(_say_path + "NameBox/Label")
+onready var _ren_input = get_node(_say_path + "Input")
+
+var itest
+var mat = 12 + 34
 
 func _ready():
-	say("developer", "This is {b}just{/b} a {i}test{/i} [12+34].")
+	_ren.define("mat", mat)
+	print(Globals.get("vars"))
+	_test()
+	#pass
 
-func input(what, temp):
-	_say_screen.clear()
-	_say_screen.set_hidden(True)
-	_namebox_screen.clear()
-
-	_input.clear()
-	_input.set_hidden(False)
-
-	_bbcode = sentence_passer(temp)
-
-	_input.set_bbcode(_bbcode)
-
-	_bbcode = sentence_passer(what)
-	_namebox_screen.set_bbcode(_bbcode)
-
-	return _input.get_bbcode()
-
+func _test():
+	say("developer", "This is {b}just{/b} a {i}test{/i} [mat].")
 
 func say(how, what):
-	#print("clear say screen")
-	_say_screen.clear()
-	_say_screen.set_hidden(False)
-	_namebox_screen.clear()
+	_say_screen.set_hidden(false)
+	_ren_input.set_hidden(true)
 
-	#print("setting say screen text")
-	_bbcode = sentence_passer(how)
+	_bbcode = _ren.sentence_passer(how)
 	_namebox_screen.set_bbcode(_bbcode)
 
-	_bbcode = sentence_passer(what)
-	_say_screen.set_bbcode()
-
-func sentence_passer(text):
-	#print("parse text")
-
-	var	pstr = ""
-	var vstr = ""
-	var b = false
-
-	var i = 1
-
-	for t in text:
-
-		if t == "[":
-			b = true
-			pstr += t
-
-		elif t == "]":
-			b = false
-			pstr += t
-			vstr = var2str(str2var(vstr))
-			print(pstr, " ", vstr)
-			text = text.replace(pstr, vstr)
-
-		elif b:
-			pstr += t
-			vstr += t
-
-	text = text.replace("{image", "[img")
-	text = text.replace("{", "[")
-	text = text.replace("}", "]")
-
-	return text
+	_bbcode = _ren.sentence_passer(what)
+	_say_screen.set_bbcode(_bbcode)
