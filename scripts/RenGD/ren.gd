197caa61e4e40c9e8b8b5bf0cbaa5df5df92d85f
jeremi360 <jeremi360@gmail.com>
2017-01-08 09:18:45 +0100

    Change funcs to class to make easy to implement scrollback

--- scripts/RenGD/ren.gd
+++ scripts/RenGD/ren.gd
@@ -13,81 +13,60 @@ onready var _say_screen = get_node("Say/VBoxContainer/Dialog")
 onready var _namebox_screen = get_node("Say/VBoxContainer/NameBox/Label")
 onready var _input = get_node("Say/VBoxContainer/Input")
 
-var _characters = []
-var _scenes = []
-var _nodes = []
-var _vars = {}
-var _args = []
+# var characters = []
+var objects = []
+# var labels = []
+var vars = {}
 
-var quotes = "\""
-var apost = "\'"
-var lend = "\n"
-var tab = "\t"
-
-var _bbcode = ""
 var _is_input_on = false
 var _input_var
-
 
 func _ready():
     _input_screen.connect("text_entered", self, "_on_input")
 
-    print("test line passer:")
-    var line_code = line_passer("$ self.get_name()")
-    print(line_code)
-    var line_say_a = line_passer("\"developer\" \"This is just test\"")
-    print(line_say_a)
-
 func define(var_name, var_value):
-    _vars.var_name = var_value
+    vars.var_name = var_value
 
 
 func get_var():
-    return _vars.var_name
+    return vars.get_var_name()
 
 
-func line_passer(line):
-    var c_line = line.c_escape()
+class input extends Object:
+    func _init(ivar, what, temp):
+        temp = str_passer(temp)
+        what = str_passer(what)
+        ivar = ivar
+        
+        objects.append(self)
 
-    var say_pattern_a = "\"someone\" \"Say some thing.\"".c_escape()
+    func show():
+        _input.set_text(temp)
+        _namebox_screen.set_bbcode(what)
 
-    if line.begins_with("$"):
-        line = line.replace("$", "")
-
-    elif c_line.begins_with(quotes):
-        #or c_line.begins_with(apost):
-        line = line + " sim: " + var2str(c_line.similarity(say_pattern_a))
-
-    return line
-
-
-func input(ivar, what, temp):
-    _bbcode = str_passer(temp)
-    set_text(_bbcode)
-
-    _bbcode = str_passer(what)
-    _namebox_screen.set_bbcode(_bbcode)
-
-    _say_screen.hide()
-    show()
-    _input_var = ivar
-    _is_input_on = true
+        _say_screen.hide()
+        _input.show()
+        _input_var = ivar
+        _is_input_on = true
 
 
 func _on_input(s):
     set(_input_var, s)
 
 
-func say(how, what):
-    _say_screen.show()
-    _input.hide()
+class say extends Object:
+    func _init(how, what):
+        how = say_passer(how)
+        what = say_passer(what)
+       
+        objects.append(self)
 
-    _bbcode = str_passer(how)
-    _namebox_screen.set_bbcode(_bbcode)
-
-    _bbcode = str_passer(what)
-    _say_screen.set_bbcode(_bbcode)
-
+    func show():
+        _namebox_screen.set_bbcode(how)
+        _say_screen.set_bbcode(what)
+        
+        _say_screen.show()
+        _input.hide()
 
 func say_passer(text):
     var pstr = ""
@@ -113,5 +92,5 @@ func say_passer(text):
     text = text.replace("{image", "[img")
     text = text.replace("{", "[")
     text = text.replace("}", "]")
-    
+
     return text
---
+++ scripts/RenGD/ren_scene.gd
@@ -0,0 +1,39 @@
+## This is Ren'GD API ##
+## Ren'GD is Ren'Py for Godot ##
+## version: 0.0.2 ##
+## License MIT ##
+## Copyright (c) 2016 Jeremi Biernacki ##
+## This part is hold on and don't work yet ##
+
+extends Node
+
+
+# var _scenes = []
+# var _nodes = []
+# var _args = []
+
+
+func quotes():
+    return "\"".c_escape()
+
+func apost():
+    return "\'".c_escape()
+
+func lend():
+    return "\n".c_escape()
+
+func tab():
+    return "\t".c_escape()
+
+func _ready():
+    _input_screen.connect("text_entered", self, "_on_input")
+
+    print("test line passer:")
+    var line_code = line_passer("$ self.get_name()")
+    print(line_code)
+
+    var line_say_a = line_passer("\"developer\" \"This is just test\"")
+    print(line_say_a)
+
+    var line_say_b = line_passer("\'developer\' \'This is just test\'")
+    print(line_say_b)
