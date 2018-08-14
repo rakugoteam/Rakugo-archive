bed0197143a64ebb9ca5546b92673f8d8c94550b
Jeremi Biernacki <jeremi360@gmail.com>
2018-08-14 20:59:31 +0200

    better RenSayPanel

--- addons/Ren/nodes/ren_say_panel.gd
+++ addons/Ren/nodes/ren_say_panel.gd
@@ -3,33 +3,55 @@ extends Panel
 export(NodePath) var name_label_path = NodePath("")
 export(NodePath) var dialog_label_path = NodePath("")
 export(NodePath) var avatar_viewport_path = NodePath("")
+export(float) var step_time = 0.01
+export(float) var letter_speed = 0.005
 
 onready var NameLabel = get_node(name_label_path)
 onready var DialogText = get_node(dialog_label_path)
 onready var CharacterAvatar = get_node(avatar_viewport_path)
-
+onready var ActionTimer = Timer.new()
+onready var DialogTimer = Timer.new()
 
 var avatar_path = ""
 var avatar
 var _type
-var dialog_timer
 var typing = false
+var active = true
 
 
 func _ready():
 	connect("gui_input", self, "_on_Adv_gui_input")
 	Ren.connect("exec_statement", self, "_on_statement")
+	ActionTimer.one_shot = true
+	ActionTimer.wait_time = step_time
+	ActionTimer.connect("timeout", self, "_on_time_active_timeout")
+	add_child(ActionTimer)
+	DialogTimer.one_shot = true
+	add_child(DialogTimer)
+
+func _on_time_active_timeout():
+	active = true
 
 func _input(event):
+	if not event.is_action_pressed("ren_forward"):
+		return
+	
+	if ActionTimer.is_stopped():
+		active = false
+		ActionTimer.start()
+
 	if Ren.skip_auto:
+		Ren.skip_auto = true
 		return
 
-	if event.is_action_pressed("ren_forward"):
-		if typing: # if typing complete it
-			typing = false
+	elif typing: # if typing complete it
+		typing = false
+		return
 
-		elif _type == "say": # else exit statement
-			Ren.exit_statement()
+	elif _type == "say": # else exit statement
+		active = true
+		ActionTimer.stop()
+		Ren.exit_statement()
 
 
 func _on_statement(type, kwargs):
@@ -72,37 +94,27 @@ func _on_statement(type, kwargs):
 		
 	return
 
-func new_timer(time):
-	var nt = Timer.new()
-	nt.set_wait_time(time)
-	nt.set_one_shot(true)
-	return nt
-
-func writeDialog(text, speed=0.005):
-	# create a timer to print text like a typewriter
-	if dialog_timer != null:
-		dialog_timer.free()
-
+func writeDialog(text, speed = letter_speed):
 	if speed == 0:
 		if DialogText.has_method("set_bbcode"):
-			DialogText.bbcode_text=  text
+			DialogText.bbcode_text = text
 		return
 
 	typing = true
 	if DialogText.has_method("set_bbcode"):
 		DialogText.bbcode_text = ""
+
 	var te = ""
-	dialog_timer = new_timer(speed)
-	self.add_child(dialog_timer)
+	DialogTimer.wait_time = speed
 
 	for letter in text:
-		dialog_timer.start()
+		DialogTimer.start()
 		te += letter
 
 		if DialogText.has_method("set_bbcode"):
 			DialogText.bbcode_text = te
 
-		yield(dialog_timer, "timeout")
+		yield(DialogTimer, "timeout")
 		if !typing:
 
 			if DialogText.has_method("set_bbcode"):
@@ -112,6 +124,9 @@ func writeDialog(text, speed=0.005):
 
 
 func _on_Adv_gui_input(ev):
+	if not active:
+		return
+
 	if not (ev is InputEventMouseButton):
 		return
 
