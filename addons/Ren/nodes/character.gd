48eea1ca3f3e5be726460ffee4fe99909ad7d665
Jeremi Biernacki <jeremi360@gmail.com>
2018-09-24 17:08:29 +0200

    more character fixes part2

--- addons/Ren/nodes/character.gd
+++ addons/Ren/nodes/character.gd
@@ -3,30 +3,40 @@ extends Node
 var kws = ["name", "color", "prefix", "suffix", "avatar"]
 var _color
 var _avatar
+var _id
 var type setget _get_type
 var value setget dict2character, character2dict
 
 export(String) var character_id = ""
 export(String) var character_name = ""
-export(Color) var color = Color("#ffffff") setget set_color, get_color
+export(Color) var color = Color("#ffffff") setget _set_color, _get_color
 export(String) var prefix = ""
 export(String) var suffix = ""
-export(PackedScene) var avatar setget set_avatar, get_avatar
+export(PackedScene) var avatar setget _set_avatar, _get_avatar
 
 func _ready():
 	Ren.variables[character_id] = self
 	Ren.debug(character2dict(), kws, "Add Character " + character_id + " with ")
 
-func set_color(value):
-	_color = value.to_html()
+func _set_character_id(id):
+	if Ren.variables.has(_id):
+		Ren.variables.erase(_id)
+	_id = character_id
+	Ren.variables[_id] = self
 
-func get_color():
+func _get_character_id():
+	return _id
+
+func _set_color(vcolor):
+	_color = vcolor.to_html()
+
+func _get_color():
 	return _color
 
-func set_avatar(value):
-	_avatar = value.resource_path
+func _set_avatar(vavatar):
+	_avatar = vavatar.resource_path
 	
-func get_avatar():
+func _get_avatar():
 	return _avatar
 
 func parse_character():
