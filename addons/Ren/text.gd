7146fd01295e16958b8c832b137ff25fb827b2c8
Jeremi Biernacki <jeremi360@gmail.com>
2018-11-29 23:34:18 +0100

    simple add of bbcode mode to text_passer

--- addons/Ren/text.gd
+++ addons/Ren/text.gd
@@ -1,9 +1,13 @@
 extends Object
 
-func text_passer(text, variables):
-	## passer for renpy markup format
-	## its retrun bbcode
-	# var _text = text
+func text_passer(text, variables, mode = "ren"):
+	match mode:
+		"ren":
+			return renpy_passer(text, variables)
+		"bbcode":
+			return bbcode_passer(text, variables)
+
+func vars_passer(text, variables, open_char, close_char):
 	if text != "":
 
 		## clean from tabs
@@ -22,13 +26,13 @@ func text_passer(text, variables):
 			var type = variables[var_name].type
 
 			# Ren.debug([var_name, type, value])
-			var s = "[" + var_name + "]"
+			var s = open_char + var_name + close_char
 			if type == Ren.Type.TEXT:
 				text = text.replace(s, value)
 			
-#			elif type == "func":
-#				var func_result = call(variable)
-#				text = text.replace("[" + var_name + "()]", str(func_result))
+			# elif type == "func":
+			# 	var func_result = call(variable)
+			# 	text = text.replace(open_char + var_name + "()]", str(func_result))
 			
 			elif type == Ren.Type.VAR:
 				text = text.replace(s, str(value))
@@ -39,14 +43,14 @@ func text_passer(text, variables):
 				text = text.replace(s, str(dict))
 				
 				for k in dict.keys():
-					var sk = "[" + var_name + "." + k + "]"
+					var sk = open_char + var_name + "." + k + close_char
 					if text.find(sk) == -1:
 						continue # no variable in this string
 					
 					var kvalue = str(dict[k])
 					# need testing
-					# if k == "name" and type == Ren.Type.CHARACTER:
-					# 	kvalue = dict.parse_character()
+					if k == "name" and type == Ren.Type.CHARACTER:
+						kvalue = dict.parse_character()
 					
 					text = text.replace(sk, kvalue)
 			
@@ -54,7 +58,7 @@ func text_passer(text, variables):
 				text = text.replace(s, str(value))
 				
 				for i in range(value.size()):
-					var sa = "[" + var_name + "[" + str(i) + "]]"
+					var sa = open_char + var_name + "[" + str(i) + "]" + close_char
 					if text.find(sa) == -1:
 						continue # no variable in this string
 					
@@ -62,14 +66,35 @@ func text_passer(text, variables):
 
 			else:
 				print(var_name," is unsuported variable type: ", type)
-			
-		text = text.replace("{image", "[img")
-		text = text.replace("{a=", "[url=")
-		text = text.replace("{/a}", "[/url]")
-		text = text.replace("{nl}", "\n")
-		text = text.replace("{tab}", "\t")
-		text = text.replace("{", "[")
-		text = text.replace("}", "]")
-		# Ren.debug("org: ''", _text, "', bbcode: ''", text , "'")
+		
+		return text
+
+func renpy_passer(text, variables):
+	## passer for renpy markup format
+	## its return bbcode
+
+	text = vars_passer(text, variables, "[", "]")
+	text = text.replace("{image", "[img")
+	text = text.replace("{a=", "[url=")
+	text = text.replace("{/a}", "[/url]")
+	text = text.replace("{nl}", "\n")
+	text = text.replace("{tab}", "\t")
+	text = text.replace("{", "[")
+	text = text.replace("}", "]")
+
+	# Ren.debug("org: ''", _text, "', bbcode: ''", text , "'")
+
+	return text
+
+
+func bbcode_passer(text, variables):
+	## passer for renpy markup format
+	## its return bbcode
+
+	text = vars_passer(text, variables, "{", "}")
+	text = text.replace("[nl]", "\n")
+	text = text.replace("[tab]", "\t")
+
+	# Ren.debug("org: ''", _text, "', bbcode: ''", text , "'")
 
 	return text
