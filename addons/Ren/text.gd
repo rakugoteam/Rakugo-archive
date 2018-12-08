c03a020a880a6d1e8e3341c4727f9eebf86574e2
Jeremi Biernacki <jeremi360@gmail.com>
2018-12-08 18:08:20 +0100

    split parse_text to 2 funcs

--- addons/Ren/text.gd
+++ addons/Ren/text.gd
@@ -4,72 +4,77 @@ func text_passer(text, variables):
 	## passer for renpy markup format
 	## its retrun bbcode
 	# var _text = text
-	if text != "":
+	if text == "":
+		return text
 
-		## clean from tabs
-		text = text.c_escape()
-		text = text.replace("\t".c_escape(), "")
-		text = text.c_unescape()
+	text = parse_text_adv(text, variables, "[", "]")
+		
+	text = text.replace("{image", "[img")
+	text = text.replace("{a=", "[url=")
+	text = text.replace("{/a}", "[/url]")
+	text = text.replace("{/nl}", "\n")
+	text = text.replace("{/tab}", "\t")
+	text = text.replace("{", "[")
+	text = text.replace("}", "]")
+	# Ren.debug("org: ''", _text, "', bbcode: ''", text , "'")
 
-		## code from Sebastian Holc solution:
-		## http://pastebin.com/K8zsWQtL
+	return text
 
-		for var_name in variables.keys():
-			if text.find(var_name) == -1:
-				continue # no variable in this string
+func parse_text_adv(text, variables, open, close):
+	## clean from tabs
+	text = text.c_escape()
+	text = text.replace("\t".c_escape(), "")
+	text = text.c_unescape()
+
+	## code from Sebastian Holc solution:
+	## http://pastebin.com/K8zsWQtL
+
+	for var_name in variables.keys():
+		if text.find(var_name) == -1:
+			continue # no variable in this string
+		
+		var value = variables[var_name].value
+		var type = variables[var_name].type
+
+		# Ren.debug([var_name, type, value])
+		var s = open + var_name + close
+		if type == Ren.Type.TEXT:
+			text = text.replace(s, value)
+		
+		elif type == Ren.Type.VAR:
+			text = text.replace(s, str(value))
+		
+		elif type in [Ren.Type.DICT, Ren.Type.CHARACTER]:
+			var dict = value
+
+			text = text.replace(s, str(dict))
 			
-			var value = variables[var_name].value
-			var type = variables[var_name].type
-
-			# Ren.debug([var_name, type, value])
-			var s = "[" + var_name + "]"
-			if type == Ren.Type.TEXT:
-				text = text.replace(s, value)
-			
-#			elif type == "func":
-#				var func_result = call(variable)
-#				text = text.replace("[" + var_name + "()]", str(func_result))
-			
-			elif type == Ren.Type.VAR:
-				text = text.replace(s, str(value))
-			
-			elif type in [Ren.Type.DICT, Ren.Type.CHARACTER]:
-				var dict = value
-
-				text = text.replace(s, str(dict))
+			for k in dict.keys():
+				var sk = open + var_name + "." + k + close
+				if text.find(sk) == -1:
+					continue # no variable in this string
 				
-				for k in dict.keys():
-					var sk = "[" + var_name + "." + k + "]"
-					if text.find(sk) == -1:
-						continue # no variable in this string
-					
-					var kvalue = str(dict[k])
-					# need testing
-					if k == "name" and type == Ren.Type.CHARACTER:
-						kvalue = variables[var_name].parse_character()
-					
-					text = text.replace(sk, kvalue)
-			
-			elif type == Ren.Type.LIST:
-				text = text.replace(s, str(value))
+				var kvalue = str(dict[k])
+
+				if k == "name" and type == Ren.Type.CHARACTER:
+					kvalue = variables[var_name].parse_character()
 				
-				for i in range(value.size()):
-					var sa = "[" + var_name + "[" + str(i) + "]]"
-					if text.find(sa) == -1:
-						continue # no variable in this string
-					
-					text = text.replace(sa, str(value[i]))
-
-			else:
-				print(var_name," is unsuported variable type: ", type)
+				# if "()" in k:
+				# 	kvalue = variables[var_name].callv()
+				
+				text = text.replace(sk, kvalue)
+		
+		elif type == Ren.Type.LIST:
+			text = text.replace(s, str(value))
 			
-		text = text.replace("{image", "[img")
-		text = text.replace("{a=", "[url=")
-		text = text.replace("{/a}", "[/url]")
-		text = text.replace("{/nl}", "\n")
-		text = text.replace("{/tab}", "\t")
-		text = text.replace("{", "[")
-		text = text.replace("}", "]")
-		# Ren.debug("org: ''", _text, "', bbcode: ''", text , "'")
+			for i in range(value.size()):
+				var sa = open + var_name + open + str(i) + close + close
+				if text.find(sa) == -1:
+					continue # no variable in this string
+				
+				text = text.replace(sa, str(value[i]))
 
+		else:
+			print(var_name," is unsuported variable type: ", type)
+		
 	return text
