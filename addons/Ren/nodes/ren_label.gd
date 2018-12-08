350da9b7a7c2e1b3313cd37850ab224fbd9dc548
Jeremi Biernacki <jeremi360@gmail.com>
2018-12-08 20:14:17 +0100

    better RenTextLabel

--- addons/Ren/nodes/ren_label.gd
+++ addons/Ren/nodes/ren_label.gd
@@ -1,17 +1,14 @@
 extends RichTextLabel
 
 export(String, FILE, "*.txt") var ren_text_file = ""
-export(String, MULTILINE) var ren_text = "" setget set_ren_text, get_ren_text
+export(String, MULTILINE) var ren_text = ""
 export(Array, String) var vars_names = []
+
+var file = File.new()
 
 func _ready():
 	bbcode_enabled = true
-	if not ren_text_file.empty():
-		var file = File.new()
-		file.open(ren_text_file, file.READ)
-		ren_text = file.get_as_text()
-		file.close()
-	
+	set_ren_file(ren_text_file)
 	update_label()
 	
 	for vn in vars_names:
@@ -28,9 +25,11 @@ func on_meta_clicked(meta):
 func on_value_changed(new_value):
 	update_label()
 
-func set_ren_text(value):
-	ren_text = value
+func set_ren_file(value):
+	if value.empty():
+		return
+		
+	file.open(value, file.READ)
+	ren_text = file.get_as_text()
+	file.close()
 	update_label()
-
-func get_ren_text():
-	return ren_text
