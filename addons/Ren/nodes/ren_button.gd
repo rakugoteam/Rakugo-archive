6a00e73c26b144b7d4fa4467882ac5fa826f6426
Jeremi Biernacki <jeremi360@gmail.com>
2019-01-20 15:21:59 +0100

    RenButtons use optional typing

--- addons/Ren/nodes/ren_button.gd
+++ addons/Ren/nodes/ren_button.gd
@@ -6,10 +6,10 @@ export(bool) var auto_resize_x = true
 export(bool) var auto_resize_y = true
 
 
-func _ready():
+func _ready() -> void:
 	node_to_change = get_node(node_path)
 
-func _on_resized():
+func _on_resized() -> void:
 	if auto_resize_x:
 		node_to_change.rect_size.x = rect_size.x
 	
--- addons/Ren/nodes/ren_choice_button.gd
+++ addons/Ren/nodes/ren_choice_button.gd
@@ -1,11 +1,10 @@
-# extends "ren_base_button.gd"
 extends RenBaseButton
 class_name RenChoiceButton
 
 onready var label = RichTextLabel.new()
 var id = -1
 
-func _ready():
+func _ready() -> void:
 	disconnect("pressed", self, "_on_pressed")
 	connect("pressed", self, "_on_pressed", [], CONNECT_ONESHOT)
 	label.mouse_filter = MOUSE_FILTER_IGNORE
@@ -13,7 +12,7 @@ func _ready():
 	add_child(label)
 	node_to_change = label
 
-func _on_pressed():
+func _on_pressed() -> void:
 	._on_pressed()
 	Ren.debug(["final_choice", id])
 	Ren.exit_statement({"final_choice":id})
