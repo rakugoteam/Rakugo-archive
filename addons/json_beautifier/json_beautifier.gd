4d488b32594e1a5df52910fcd5412c21299fd3ae
Jeremi Biernacki <jeremi360@gmail.com>
2019-01-27 18:21:16 +0100

    manuall fix types in project.godot

--- addons/json_beautifier/json_beautifier.gd
+++ addons/json_beautifier/json_beautifier.gd
@@ -7,7 +7,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.                    #
 ###############################################################################
 
-extends Node
+extends Object
 class_name JSONBeautifier
 
 # Takes valid JSON (if invalid, it will return a error according with Godot's
