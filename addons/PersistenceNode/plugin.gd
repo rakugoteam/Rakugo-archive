1995fa163b9b8f5ec4035d23f8afd413537a881d
Jeremi Biernacki <jeremi360@gmail.com>
2019-03-24 12:04:36 +0100

    remove PersistanceNode and JSONBeautifier addons

--- addons/PersistenceNode/plugin.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-#func _enter_tree():
-#	add_custom_type("Persistence", "Node", preload("persistence_node.gd"), preload("icon.svg"))
-#	# add_autoload_singleton(name, path) # 3.1
-#
-#func _exit_tree():
-#	remove_custom_type("Persistence")
