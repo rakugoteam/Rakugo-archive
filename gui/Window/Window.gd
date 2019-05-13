16a97d41dfb867e9f61f597573dd5d0f2056c025
Jeremi Biernacki <jeremi360@gmail.com>
2019-05-13 20:45:35 +0200

    Window manual resize fix

--- gui/Window/Window.gd
+++ gui/Window/Window.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
+var default
+var prev
+
 func _ready() -> void:
-	var default = settings.default_window_size
+	default = settings.default_window_size
+	prev = default
 	scale = OS.window_size / default
 	settings.connect("window_size_changed", self, "_on_window_size_changed")
 
 func _on_window_size_changed(prev : Vector2, now : Vector2) -> void:
-	var default = settings.default_window_size
 	scale = now / default
+	prev = prev
 
 func _process(delta: float) -> void:
 	var now = OS.window_size
-	var default = settings.default_window_size
-	scale = now / default
+	
+	if prev != now:
+		prev = now
+		scale = now / default
+		settings.temp_window_size = now
+		settings.apply(true)
+		
