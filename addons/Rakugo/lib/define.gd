0c494f6f2b13b19f05f37d570f1c0da0f1a2668c
Jeremi Biernacki <jeremi360@gmail.com>
2019-06-06 22:48:55 +0200

    added RakugoVector2, RakugoVectro3, RakugoColor

--- addons/Rakugo/lib/define.gd
+++ addons/Rakugo/lib/define.gd
@@ -2,36 +2,40 @@ extends Object
 
 func invoke(var_name:String, value, save_included: bool) -> RakugoVar:
 	var variables = Rakugo.variables
-	
+
 	if not variables.has(var_name):
 		var type = typeof(value)
-		
+
 		match type:
 			TYPE_INT:
 				return _rakugo_var(var_name, value, save_included, variables)
-				
+
 			TYPE_REAL:
 				return _rakugo_var(var_name, value, save_included, variables)
-				
+
 			TYPE_BOOL:
 				var new_var = RakugoBool.new(var_name, value)
 				new_var.save_included = save_included
 				variables[var_name] = new_var
 				return new_var as RakugoBool
-			
+
 			TYPE_ARRAY:
 				var new_var = RakugoList.new(var_name, value)
 				new_var.save_included = save_included
 				variables[var_name] = new_var
 				return new_var as RakugoList
-			
+
 			TYPE_DICTIONARY:
 				var new_var = RakugoDict.new(var_name, value)
 				new_var.save_included = save_included
 				variables[var_name] = new_var
 				return new_var as RakugoDict
-			
+
 			TYPE_STRING:
+				if value.is_valid_html_color():
+					var c = _color(var_name, value.to_html())
+					return c as RakugoColor
+					
 				var new_var = RakugoText.new(var_name, value)
 				new_var.save_included = save_included
 				variables[var_name] = new_var
@@ -39,8 +43,24 @@ func invoke(var_name:String, value, save_included: bool) -> RakugoVar:
 
 			TYPE_NODE_PATH:
 				return node_link(var_name, value, variables) as NodeLink
-				
-			_: 
+
+			TYPE_VECTOR2:
+				var new_var = RakugoVector2.new(var_name, value)
+				new_var.save_included = save_included
+				variables[var_name] = new_var
+				return new_var as RakugoVector2
+
+			TYPE_VECTOR3:
+				var new_var = RakugoVector3.new(var_name, value)
+				new_var.save_included = save_included
+				variables[var_name] = new_var
+				return new_var as RakugoVector3
+
+			TYPE_COLOR:
+				var c = _color(var_name, value.to_html())
+				return c as RakugoColor
+
+			_:
 				return null
 
 	return null
@@ -49,10 +69,16 @@ func _rakugo_var(var_name:String , value, save_included: bool, variables: Dictio
 	var new_var = RakugoVar.new(var_name, value)
 	new_var.save_included = save_included
 	variables[var_name] = new_var
-	return new_var 
+	return new_var
 
 func node_link(node_id:String, node_path: NodePath, variables:Dictionary) -> NodeLink:
 	var node_var = NodeLink.new(node_id)
 	node_var.value["node_path"] = node_path
 	variables[node_var.id] = node_var
 	return node_var
+
+func _color(var_name:String , value:String, save_included: bool) -> RakugoColor:
+	var new_var = RakugoColor.new(var_name, value)
+	new_var.save_included = save_included
+	variables[var_name] = new_var
+	return new_var
---
+++ addons/Rakugo/types/rakugo_vector3.gd
@@ -0,0 +1,86 @@
+extends RakugoVector2
+class_name RakugoVector3
+
+var z : float setget _set_z, _get_z
+
+func _init(var_id : String, var_value : Vector3
+	).(var_id, var_value, Rakugo.Type.VECT3):
+	pass
+
+func _set_value(v : Vector3) -> void:
+	._set_value(v)
+
+func _get_value() -> Vector3:
+	return _value
+
+func _set_z(_z : float) -> void:
+	_value.z = _z
+
+func _get_z() -> float:
+	return _value.z
+
+func abs () -> Vector3:
+	return _value.abs()
+
+func angle_to ( to : Vector3 ) -> float:
+	return _value.angle_to(to)
+
+func angle_to_point ( to : Vector3 ) -> float:
+	return _value.angle_to_point(to)
+
+func bounce ( n : Vector3 ) -> Vector3:
+	return _value.bounce(n)
+
+func ceil () -> Vector3:
+	return _value.ceil()
+
+func clamped ( length : float ) -> Vector3:
+	return _value.clamped(length)
+
+func cross ( with : Vector3 ) -> float:
+	return _value.cross(with)
+
+func cubic_interpolate ( b : Vector3, pre_a : Vector3, post_b : Vector3, t : float ) -> Vector3:
+	return _value.cubic_interpolate(b, pre_a, post_b, t)
+
+func distance_squared_to ( to : Vector3 ) -> float:
+	return _value.distance_squared_to(to)
+
+func distance_to ( to : Vector3 ) -> float:
+	return _value.distance_to(to)
+
+func dot ( with : Vector3 ) -> float:
+	return _value.dot(with)
+
+func floor () -> Vector3:
+	return _value.floor()
+
+func linear_interpolate ( b : Vector3, t : float ) -> Vector3:
+	return _value.linear_interpolate(b, t)
+
+func normalized () -> Vector3:
+	return _value.normalized()
+
+func project ( b : Vector3 ) -> Vector3:
+	return _value.project(b)
+
+func reflect ( n : Vector3 ) -> Vector3:
+	return _value.reflect(n)
+
+func rotated ( phi : float ) -> Vector3:
+	return _value.rotated(phi)
+
+func round () -> Vector3:
+	return _value.round()
+
+func slerp ( b : Vector3, t : float ) -> Vector3:
+	return _value.slerp(b, t)
+
+func slide ( n : Vector3 ) -> Vector3:
+	return _value.slide(n)
+
+func snapped ( by : Vector3 ) -> Vector3:
+	return _value.snapped(by)
+
+func tangent () -> Vector3:
+	return _value.tangent()
