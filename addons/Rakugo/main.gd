3f455737558fbc03049e76a13a9b60b081715b93
Jeremi Biernacki <jeremi360@gmail.com>
2020-02-06 18:20:32 +0100

    cleanup and fix after PR #248 + fixed links in credits.txt

--- addons/Rakugo/main.gd
+++ addons/Rakugo/main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const rakugo_version := "2.0.8"
+const rakugo_version := "2.0.8f4"
 const credits_path := "res://addons/Rakugo/credits.txt"
 
 # we need it because we hide base RakugoMenu form custom nodes
