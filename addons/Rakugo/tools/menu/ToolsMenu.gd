7c7d7039e0d10c61b2ca3f22916d47e5a30c2aff
Jeremi Biernacki <jeremi360@gmail.com>
2020-03-15 13:10:09 +0100

    Better Rakugo tools
    - Move Rakugo Project Setting to Project Settings
    - Bring Back Rakugo Tool Menu
    - Restore missing SomeDialog.gd
    - Fix Screen.gd
    - Clean up
    - update credits text

---
+++ addons/Rakugo/tools/menu/ToolsMenu.gd
@@ -0,0 +1,29 @@
+tool
+extends MenuButton
+
+var plugin:EditorPlugin
+
+func _ready() -> void:
+	var docs_icon := get_icon("Help", "EditorIcons")
+	get_popup().set_item_icon(2, docs_icon)
+
+	get_popup().connect("id_pressed", self, "_on_id")
+
+
+func _on_id(id:int) -> void:
+	match id:
+		# Emoji Panel
+		0:
+			plugin.emoji_panel.popup_centered()
+
+		# ScenesLinks Tool
+		1:
+			plugin.sl_tool.popup_centered()
+
+		# Rakugo Docs
+		2:
+			plugin.open_rakugo_docs()
+
+		# About Rakugo
+		3:
+			plugin.open_about_dialog()
