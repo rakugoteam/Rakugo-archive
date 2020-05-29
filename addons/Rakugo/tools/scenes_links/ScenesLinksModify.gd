9ceb86a5409feabb5aaa79d125a4dbb79c94f309
Jeremi Biernacki <jeremi360@gmail.com>
2020-05-29 13:47:23 +0200

    add and make use of new class NotifyLabel

--- addons/Rakugo/tools/scenes_links/ScenesLinksModify.gd
+++ addons/Rakugo/tools/scenes_links/ScenesLinksModify.gd
@@ -23,10 +23,8 @@ func _ready() -> void:
 	$ScenesLinksChooser.connect("new_file", self, "_on_new")
 	$ScenesLinksChooser.connect("cancel", self, "_on_cancel")
 	$ScenesLinksChooser.connect("apply", self, "_on_apply")
-	$ScenesLinksChooser.connect("set_as_def", self, "notify",
-	 ["Setted as Default "])
+	$ScenesLinksChooser.connect("set_as_def", self, "notify", ["Setted as Default "])
 	fd.connect("confirmed", self, "_on_file_dialog")
-	tween.connect("tween_all_completed", $Label, "hide")
 	get_parent().connect("about_to_show", self, "_on_about_to_show")
 
 
@@ -36,21 +34,6 @@ func _on_about_to_show() -> void:
 
 func plugin_ready(_editor:EditorInterface) -> void:
 	editor = _editor
-
-
-func notify(text:String) -> void:
-	$Label.text = text
-	var scolor = Color(0, 0, 0, 0)
-	tween.interpolate_property(
-		$Label, "modulate", scolor, Color.green,
-		1, Tween.TRANS_LINEAR,Tween.EASE_IN)
-
-	tween.interpolate_property(
-		$Label, "modulate", Color.green, scolor,
-		1, Tween.TRANS_LINEAR,Tween.EASE_IN, 0.7)
-
-	$Label.show()
-	tween.start()
 
 
 func on_add() -> void:
@@ -68,7 +51,7 @@ func add_sle(scene_id:String, scene_path:String):
 
 func _on_cancel() -> void:
 	_on_open(file_path)
-	notify("Reloaded")
+	$Label.notify("Reloaded")
 
 
 func _on_new(_file_path:String) -> void:
@@ -119,12 +102,12 @@ func save_sl(_file_path:String, _scenes_links:ScenesLinks):
 	var error := ResourceSaver.save(_file_path, _scenes_links)
 
 	if error != OK:
-		notify("error see console")
+		$Label.notify("error see console")
 		print("There was issue writing ScenesLinks to %s error_number: %s" %
 			[file_path, error])
 		return
 
-	notify("Saved")
+	$Label.notify("Saved")
 
 
 func _on_file_dialog():
