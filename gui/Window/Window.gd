64abd60ea67c2f43597b385c0026bffca3175ced
Jeremi <jeremi360@gmail.com>
2020-09-13 14:09:45 +0200

    fix after remove of RootNode

--- gui/Window/Window.gd
+++ gui/Window/Window.gd
@@ -4,3 +4,6 @@ onready var viewport := $Panel/ViewportContainer/Viewport
 onready var Screens := $Panel/Screens
 onready var InGameGUI := $Panel/InGameGUI
 onready var Loading := $Panel/Loading
+
+func _ready():
+	Rakugo.current_root_node = self
