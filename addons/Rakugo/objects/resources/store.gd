67392c87ebdb736de150943480d86ce16aea5025
Vaasref <vaasref@gmail.com>
2020-09-27 18:33:57 +0200

    Overriding the duplicate() for Store to make them duplicate deeply.

--- addons/Rakugo/objects/resources/store.gd
+++ addons/Rakugo/objects/resources/store.gd
@@ -10,13 +10,15 @@ export var data := {}
 export var properties := {}
 var property_signals := []
 
+
 func _get(property):
 	if property in self.get_property_list():
 		return self[property]
 	elif self.properties.has(property):
 		return self.properties[property]
 	return null
-	
+
+
 func _set(property, value):
 	if property in self.get_property_list():
 		self[property] = value
@@ -26,15 +28,18 @@ func _set(property, value):
 		emit_signal("on_%s_changed" % property, property, value)
 	return true
 
+
 func connect_on_property_changed(property:String, target:Object, method:String):
 	if not self.has_signal("on_%s_changed" % property):
 		property_signals.append(property)
 		self.add_user_signal("on_%s_changed" % property, [{'name':'property', 'type':TYPE_STRING}, {'name':'value', 'type':TYPE_NIL}])
 	self.connect("on_%s_changed" % property, target, method)
 
+
 func disconnect_on_property_changed(property:String, target:Object, method:String):
 	if self.is_connected("on_%s_changed" % property, target, method):
 		self.disconnect("on_%s_changed" % property, target, method)
+
 
 func replace_connections(new_store):
 	new_store.property_signals = self.property_signals.duplicate(true)
@@ -46,9 +51,12 @@ func replace_connections(new_store):
 			new_store.connect(s, c.target, c.method)
 			self.disconnect(s, c.target, c.method)
 	pass
-	
-#func duplicate(deep:bool=true) -> Resource:
-#	var output = .duplicate(deep)
-#	print("Property signal duplication ", self.property_signals, "  ", output.property_signals)
-#	output.property_signals = output.property_signals.duplicate()
-#	return output
+
+
+func duplicate(_deep:bool=true) -> Resource:##Store duplication should always be deep
+	var output = .duplicate(true)
+	for p in self.get_property_list():
+		if p.type == TYPE_DICTIONARY or p.type == TYPE_ARRAY:
+			var a = output.get(p.name)
+			output.set(p.name, a.duplicate(true))
+	return output
