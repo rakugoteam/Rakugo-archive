b1607760fe68176ccf83fc0fb4365b53f4b722b5
Vaasref <vaasref@gmail.com>
2020-10-22 05:40:41 +0200

    Preventing errors if scene links file and parent directory not yet created.

--- addons/Rakugo/tools/scene_links/SceneLinksEditor.gd
+++ addons/Rakugo/tools/scene_links/SceneLinksEditor.gd
@@ -22,6 +22,9 @@ func init() -> void:
 		ch.queue_free()
 
 	scene_links = load(file_path)
+	if not scene_links:
+		scene_links = SceneLinks.new().duplicate()
+		Directory.new().make_dir_recursive(file_path.get_base_dir())
 	var sl_dict = scene_links.get_as_dict()
 	for k in sl_dict.keys():
 		var scene_path = sl_dict[k]
